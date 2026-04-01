352cee55ff04ebbf45a7d93d7914cb89edd4ec34
Khushee <230701276@rajalakshmi.edu.in>
2026-04-01 21:30:33 +0530

    Fix vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
-cat > /Users/shanky/Desktop/vidlearn-ai-v3-final/frontend/vite.config.js << 'EOF'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()]
 })
-EOF
